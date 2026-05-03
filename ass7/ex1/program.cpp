25a68a9d123738b18831ae1fa026adc37544f911
EinMilchBoss <christian.benezuela@gmail.com>
2026-05-03 14:33:03 +0200

    Add recursive delete function.

--- ass7/ex1/program.cpp
+++ ass7/ex1/program.cpp
@@ -24,6 +24,7 @@ public:
     vertex &operator=(const vertex &) = delete;
     vertex(vertex &&) = delete;
     vertex &operator=(vertex &&) = delete;
+    ~vertex() = default;
 };
 
 class binary_search_tree
@@ -35,7 +36,7 @@ class binary_search_tree
         assert(new_vertex != nullptr && "we add a valid vertex");
         assert(current != nullptr && "we are inside our tree");
 
-        if (new_vertex->value_ <= current->value_)
+        if (new_vertex->value_ < current->value_)
         {
             if (current->next_left_ == nullptr)
             {
@@ -46,7 +47,7 @@ class binary_search_tree
 
             do_add(new_vertex, current->next_left_);
         }
-        else
+        else if (new_vertex->value_ > current->value_)
         {
             if (current->next_right_ == nullptr)
             {
@@ -57,6 +58,65 @@ class binary_search_tree
 
             do_add(new_vertex, current->next_right_);
         }
+        else
+        {
+            throw std::runtime_error("the same number was inserted into the binary search tree twice");
+        }
+    }
+
+    void perform_replacement(vertex *replacee, vertex **prev_next)
+    {
+        if (replacee->next_left_ == nullptr && replacee->next_right_ == nullptr)
+        {
+            // If the replacee doesn't have any subtrees, just remove the vertex and update previous vertex.
+
+            *prev_next = nullptr;
+            delete replacee;
+        }
+        else if (replacee->next_left_ != nullptr && replacee->next_right_ != nullptr)
+        {
+            // If the replacee has two subtrees, find the in-order predecessor.
+
+            vertex *replacer = leftmost(replacee->next_right_);
+
+            replacee->value_ = replacer->value_;
+
+            if (replacer == replacee->next_right_)
+                replacee->next_right_ = nullptr;
+            else
+                replacer->prev_->next_left_ = nullptr;
+            delete replacer;
+        }
+        else
+        {
+            // If the replacee has only one subtree, connect that subtree directly.
+
+            vertex *replacer =
+                replacee->next_left_ != nullptr
+                    ? replacee->next_left_
+                    : replacee->next_right_;
+
+            replacer->prev_ = replacee->prev_;
+            *prev_next = replacer;
+            delete replacee;
+        }
+    }
+
+    void do_remove(int value, vertex *current, vertex **prev_next)
+    {
+        assert(prev_next != nullptr && *prev_next == current && "the successor of the previous vertex is the current vertex");
+
+        // The value does not exist in our tree.
+        if (current == nullptr)
+            return;
+
+        if (value < current->value_)
+            do_remove(value, current->next_left_, &current->next_left_);
+
+        else if (value > current->value_)
+            do_remove(value, current->next_right_, &current->next_right_);
+        else
+            perform_replacement(current, prev_next);
     }
 
     void do_to_string_in_order(std::stringstream &ss, const vertex *current) const
@@ -74,11 +134,27 @@ class binary_search_tree
         ss << " )";
     }
 
+    vertex *leftmost(vertex *current)
+    {
+        assert(current != nullptr && "we are inside our tree");
+
+        if (current->next_left_ == nullptr)
+            return current;
+        else
+            return leftmost(current->next_left_);
+    }
+
 public:
     binary_search_tree() noexcept
         : root_(nullptr)
     {
     }
+
+    binary_search_tree(const binary_search_tree &) = delete;
+    binary_search_tree &operator=(const binary_search_tree &) = delete;
+    binary_search_tree(binary_search_tree &&) = delete;
+    binary_search_tree &operator=(binary_search_tree &&) = delete;
+    ~binary_search_tree() = default;
 
     void add(int value)
     {
@@ -88,6 +164,11 @@ public:
             root_ = new_vertex;
         else
             do_add(new_vertex, root_);
+    }
+
+    void remove(int value)
+    {
+        do_remove(value, root_, &root_);
     }
 
     std::string to_string_in_order() const
@@ -100,9 +181,6 @@ public:
 
 int main()
 {
-    // Binär verketteter Suchbaum
-    // Löschen!
-
     binary_search_tree bst;
     bst.add(1);
     bst.add(2);
@@ -110,6 +188,13 @@ int main()
     bst.add(0);
     bst.add(-2);
     bst.add(-1);
+    // bst.remove(3);
+    // bst.remove(2);
+    // bst.remove(-2);
+    // bst.remove(0);
+    // bst.remove(1);
+    // bst.remove(-1);
+    bst.remove(1);
     std::cout << bst.to_string_in_order() << "\n";
 
     return 0;
