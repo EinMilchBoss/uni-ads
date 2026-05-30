1fe5adf2b0470549cc8c828ada4f9228b9e702cd
EinMilchBoss <christian.benezuela@gmail.com>
2026-05-30 19:26:04 +0200

    Finally got it to Θ(|V| + |E|).

--- ass11/ex4/program.cpp
+++ ass11/ex4/program.cpp
@@ -3,68 +3,68 @@
 #include <array>
 #include <queue>
 
-/**
- * Complexity: Θ(|V|^2)
- */
-std::vector<size_t> get_pos_degs(const std::vector<bool> &adj_mat, size_t verts_amt)
+struct vertex
 {
-    const size_t adj_mat_size = verts_amt * verts_amt;
+    size_t idx;
+    std::vector<vertex *> succs;
+};
 
-    std::vector<size_t> pos_degs(verts_amt, 0);
+/**
+ * Complexity: Θ(|V| + |E|)
+ */
+std::vector<size_t> get_pos_degs(const std::vector<vertex> &verts)
+{
+    std::vector<size_t> pos_degs(verts.size(), 0);
 
-    for (size_t base_idx = 0; base_idx < adj_mat_size; base_idx += verts_amt)
+    for (const vertex &vert : verts)
     {
-        for (size_t vert = 0; vert < verts_amt; ++vert)
-        {
-            const size_t vert_idx = base_idx + vert;
-            if (adj_mat[vert_idx] == 1)
-                ++pos_degs[vert];
-        }
+        for (size_t i = 0; i < vert.succs.size(); ++i)
+            ++pos_degs[vert.succs[i]->idx];
     }
 
     return pos_degs;
 }
 
 /**
- * Complexity: Θ(|V|^2)
+ * Complexity: Θ(|V| + |E|)
  */
-std::vector<size_t> get_topological_order(const std::vector<bool> &adj_mat, size_t verts_amt)
+std::vector<size_t> get_topological_order(const std::vector<vertex> &verts)
 {
-    std::vector<size_t> pos_degs = get_pos_degs(adj_mat, verts_amt);
+    // Complexity: Θ(|V| + |E|)
+    std::vector<size_t> pos_degs = get_pos_degs(verts);
 
+    // Complexity: Θ(|V|)
     std::queue<size_t> queue;
-    for (size_t vert = 0; vert < verts_amt; ++vert)
+    for (size_t i = 0; i < pos_degs.size(); ++i)
     {
-        if (pos_degs[vert] == 0)
-            queue.push(vert);
+        if (pos_degs[i] == 0)
+            queue.push(i);
     }
 
     std::vector<size_t> order;
-    order.reserve(verts_amt);
+    order.reserve(verts.size());
 
-    // By the same logic of BFS,
+    // Apply BFS for all vertecies v where deg+(v) = 0.
+    // Complexity: Θ(|V| + |E|)
     while (!queue.empty())
     {
-        const size_t curr_vert = queue.front();
+        const size_t curr_vert_idx = queue.front();
         queue.pop();
 
-        // Update positive degrees of processed vertex.
-        const size_t base_idx = curr_vert * verts_amt;
-        for (size_t dst_vert = 0; dst_vert < verts_amt; ++dst_vert)
+        // Update positive degrees of successor vertecies.
+        for (size_t succ_idx = 0; succ_idx < verts[curr_vert_idx].succs.size(); ++succ_idx)
         {
-            const size_t dst_idx = base_idx + dst_vert;
-            if (adj_mat[dst_idx])
-            {
-                const size_t remaining_incoming = --pos_degs[dst_vert];
-                if (!remaining_incoming)
-                    queue.push(dst_vert);
-            }
+            const size_t succ_vert_idx = verts[curr_vert_idx].succs[succ_idx]->idx;
+
+            --pos_degs[succ_vert_idx];
+            if (pos_degs[succ_vert_idx] == 0)
+                queue.push(succ_vert_idx);
         }
 
-        order.push_back(curr_vert);
+        order.push_back(curr_vert_idx);
     }
 
-    if (order.size() < verts_amt)
+    if (order.size() < verts.size())
         throw std::runtime_error("tried to sort cyclic graph topologically");
 
     return order;
@@ -74,16 +74,14 @@ int main()
 {
     constexpr size_t adj_mat_verts_amt = 3;
 
-    /* clang-format off */
-    std::vector<bool> adj_mat = 
-    {
-        0, 1, 1,
-        0, 0, 1,
-        0, 0, 0,
-    };
-    /* clang-format on */
+    std::vector<vertex> verts(adj_mat_verts_amt, vertex{});
+    for (size_t i = 0; i < adj_mat_verts_amt; ++i)
+        verts[i].idx = i;
+    verts[0].succs.push_back(&verts[1]);
+    verts[0].succs.push_back(&verts[2]);
+    verts[1].succs.push_back(&verts[2]);
 
-    const std::vector<size_t> order = get_topological_order(adj_mat, adj_mat_verts_amt);
+    const std::vector<size_t> order = get_topological_order(verts);
     for (size_t i = 0; i < adj_mat_verts_amt; ++i)
         std::cout << "[" << i << "] " << order[i] << "\n";
 
