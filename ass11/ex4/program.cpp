c49ce8013012dbbdbd1a753d33ad5319bba321df
EinMilchBoss <christian.benezuela@gmail.com>
2026-05-30 18:10:20 +0200

    Replace scanning with queue.

--- ass11/ex4/program.cpp
+++ ass11/ex4/program.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <queue>
 
-std::vector<int16_t> get_pos_degs(const std::vector<bool> &adj_mat, size_t verts_amt)
+/**
+ * Complexity: Θ(|V|^2)
+ */
+std::vector<size_t> get_pos_degs(const std::vector<bool> &adj_mat, size_t verts_amt)
 {
     const size_t adj_mat_size = verts_amt * verts_amt;
 
-    std::vector<int16_t> pos_degs(verts_amt, 0);
+    std::vector<size_t> pos_degs(verts_amt, 0);
 
     for (size_t base_idx = 0; base_idx < adj_mat_size; base_idx += verts_amt)
     {
@@ -21,52 +25,47 @@ std::vector<int16_t> get_pos_degs(const std::vector<bool> &adj_mat, size_t verts
     return pos_degs;
 }
 
+/**
+ * Complexity: Θ(|V|^2)
+ */
 std::vector<size_t> get_topological_order(const std::vector<bool> &adj_mat, size_t verts_amt)
 {
-    constexpr int16_t already_sorted = -1;
-    std::vector<int16_t> pos_degs = get_pos_degs(adj_mat, verts_amt);
+    std::vector<size_t> pos_degs = get_pos_degs(adj_mat, verts_amt);
+
+    std::queue<size_t> queue;
+    for (size_t vert = 0; vert < verts_amt; ++vert)
+    {
+        if (pos_degs[vert] == 0)
+            queue.push(vert);
+    }
 
     std::vector<size_t> order;
     order.reserve(verts_amt);
 
-    size_t remaining_verts_amt = verts_amt;
-    size_t src_vert = 0;
-    size_t successive_skips = 0;
-    while (remaining_verts_amt > 0)
+    // By the same logic of BFS,
+    while (!queue.empty())
     {
-        if (successive_skips == remaining_verts_amt)
-            throw std::runtime_error("tried to sort cyclic graph topologically");
+        const size_t curr_vert = queue.front();
+        queue.pop();
 
-        switch (pos_degs[src_vert])
+        // Update positive degrees of processed vertex.
+        const size_t base_idx = curr_vert * verts_amt;
+        for (size_t dst_vert = 0; dst_vert < verts_amt; ++dst_vert)
         {
-        case already_sorted:
-            break;
-        case 0:
-        {
-            // Remove outgoing edges of that vertex and update positive degrees.
-            const size_t base_idx = src_vert * verts_amt;
-            for (size_t dst_vert = 0; dst_vert < verts_amt; ++dst_vert)
+            const size_t dst_idx = base_idx + dst_vert;
+            if (adj_mat[dst_idx])
             {
-                const size_t dst_idx = base_idx + dst_vert;
-                if (adj_mat[dst_idx] == 1)
-                    --pos_degs[dst_vert];
+                const size_t remaining_incoming = --pos_degs[dst_vert];
+                if (!remaining_incoming)
+                    queue.push(dst_vert);
             }
-
-            pos_degs[src_vert] = already_sorted;
-            order.push_back(src_vert);
-            --remaining_verts_amt;
-
-            successive_skips = 0;
-
-            break;
-        }
-        default:
-            ++successive_skips;
-            break;
         }
 
-        src_vert = (src_vert + 1) % verts_amt;
+        order.push_back(curr_vert);
     }
+
+    if (order.size() < verts_amt)
+        throw std::runtime_error("tried to sort cyclic graph topologically");
 
     return order;
 }
